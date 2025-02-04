1838d8a69675a4d620110170126a2dffa7600513
Flavio <flaviomatheus28013@gmail.com>
2025-02-03 23:14:40 -0300

    HeroSection

--- src/pages/homePage/HeroSection.tsx
+++ src/pages/homePage/HeroSection.tsx
@@ -1,19 +1,21 @@
 import React from "react";
+import "../../styles/pages.css";
+import PlanoDeInclusao from "../../assets/imgs/PlanoDeInclusao.jpg";
+
 
 const HeroSection = () => {
   return (
-    <section
-      className="h-screen bg-cover bg-center flex flex-col justify-center items-center text-white text-center"
+    <section className="h-screen bg-cover bg-center flex flex-col justify-center items-center text-white text-center opacity-60"
       style={{
-        backgroundImage: "url('https://via.placeholder.com/1920x1080')",
-      }}
-    >
+        backgroundImage: `url(${PlanoDeInclusao})`,
+      }}>
+
       <h1 className="text-5xl font-bold mb-4 uppercase">
         Educação grátis e acessível
       </h1>
-      <p className="text-xl mb-6">Para todo mundo</p>
+      <p className="text-xl mb-6"> PARA TODOS </p>
       <button className="bg-blue-500 px-6 py-3 rounded-md shadow-lg hover:bg-blue-700 transition-all">
-        Nos ajude a crescer
+        Inscreva-se
       </button>
     </section>
   );
